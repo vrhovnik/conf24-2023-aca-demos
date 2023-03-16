cdb7d3ac0ec5d812cc64bcc6ec9dc76d0996e2a1
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2023-03-16 13:33:27 +0100

    adding second page to demonstrate different revisions, environment as simple demonstration

---
+++ src/ITS/SimpleWebApp/Pages/SecondPage.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleWebApp.Pages;
+
+public class SecondPageModel : PageModel
+{
+    private readonly ILogger<SecondPageModel> logger;
+
+    public SecondPageModel(ILogger<SecondPageModel> logger) => this.logger = logger;
+
+    public void OnGet() => logger.LogInformation("Second page loaded at {DateLoaded}", DateTime.Now);
+}
